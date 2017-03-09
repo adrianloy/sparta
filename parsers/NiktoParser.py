058526052386b7150152b9c2b1775409d477c43c
Cedric Freiberger <cedric.freiberger@student.kit.edu>
2017-03-09 13:27:58 +0100

    improved text representation of some issues

--- parsers/NiktoParser.py
+++ parsers/NiktoParser.py
@@ -14,7 +14,7 @@ class NiktoParser(object):
 
         scan = untangle.parse(tool_node.file_output)
         for item in scan.niktoscan.scandetails.item:
-            issue_node = IssueNode(data_graph, '', '', item.uri.cdata, item.description.cdata, '', '')
+            issue_node = IssueNode(data_graph, '', '', item.uri.cdata, item.description.cdata.strip(), '', '')
             issue_node_id = tool_node.add_child(issue_node)
             data_graph.view.ui.addNodeTo(tool_node.node_id, issue_node_id, item.uri.cdata, "issues")
             data_graph.issue_dict[issue_node_id] = issue_node
