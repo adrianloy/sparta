a3e6784eeed5b55c82c0ad994609f35dc91909d4
Cedric Freiberger <cedric.freiberger@student.kit.edu>
2017-03-14 15:56:03 +0100

    now zap daemon has to be started manually

--- scripts/zap.py
+++ scripts/zap.py
@@ -16,7 +16,7 @@ if target_port == '443':
 else:
     protocol = 'http'
 
-zap = '/opt/ZAP_2.5.0/zap.sh'
+zap = 'zap.sh'
 host = '127.0.0.1'
 port = '8080'
 api_key = 'ZAPROXY-PLUGIN'
@@ -24,54 +24,39 @@ api_key = 'ZAPROXY-PLUGIN'
 port_number = int(port)
 s = socket.socket()
 connected = False
+print 'Trying to connect to ZAP daemon...'
 try:
     s.connect((host, port_number))
     print "Connected!"
     connected = True
 except Exception as e:
-    print "Couldn't connect to ZAP daemon..."
+    print "Couldn't connect!"
+    command = zap + ' -daemon -host ' + host + ' -port ' + port + ' -config api.key=' + api_key
+    print 'You need to start a ZAP daemon first:'
+    print command
 finally:
     s.close()
 
-if not connected:
-    command = 'bash ' + zap + ' -daemon -host ' + host + ' -port ' + port + ' -config api.key=' + api_key + ' > /dev/null'
-    print 'Starting ZAP daemon...'
-    print '!!! Remember to shutdown the ZAP daemon after all scans are done !!!'
-    process = subprocess.Popen(command, shell=True)
-    print 'pkill -TERM -P ' + str(process.pid)
-
-while not connected:
-    s = socket.socket()
-    try:
-        s.connect((host, port_number))
-        print "Connected!"
-        connected = True
-    except Exception as e:
-        print 'Retrying to connect in 10 sec...'
-        time.sleep(10)
-    finally:
-        s.close()
-
-zap = ZAPv2(proxies={'http': 'http://' + host + ':' + port})
-target_url = protocol + '://' + target_ip + ':' + target_port
-zap.urlopen(target_url)
-time.sleep(10)
-print 'Scanning target %s' % target_url
-print '(Report every 10 sec)'
-scan_id = zap.ascan.scan(target_url, apikey=api_key)
-while int(zap.ascan.status(scan_id)) < 100:
-    print 'Scan progress %: ' + zap.ascan.status(scan_id)
+if connected:
+    zap = ZAPv2(proxies={'http': 'http://' + host + ':' + port})
+    target_url = protocol + '://' + target_ip + ':' + target_port
+    zap.urlopen(target_url)
     time.sleep(10)
+    print 'Scanning target %s' % target_url
+    print '(Report every 10 sec)'
+    scan_id = zap.ascan.scan(target_url, apikey=api_key)
+    while int(zap.ascan.status(scan_id)) < 100:
+        print 'Scan progress %: ' + zap.ascan.status(scan_id)
+        time.sleep(10)
+    print 'Scan completed!'
 
-print 'Scan completed'
-print 'Results written in ' + output
-
-with open(output, 'w') as f:
-    f.write('<?xml version="1.0" ?>')
-    f.write('<zap>')
-    for alert in zap.core.alerts():
-        xml = dicttoxml.dicttoxml(alert, root=False)
-        f.write('<issue>')
-        f.write(xml)
-        f.write('</issue>')
-    f.write('</zap>')
+    with open(output, 'w') as f:
+        f.write('<?xml version="1.0" ?>')
+        f.write('<zap>')
+        for alert in zap.core.alerts():
+            xml = dicttoxml.dicttoxml(alert, root=False)
+            f.write('<issue>')
+            f.write(xml)
+            f.write('</issue>')
+        f.write('</zap>')
+    print 'Results written in ' + output
